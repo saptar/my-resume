85178017b453b037e205114def3ebad9f86816ea
saptarshi <saptarshidutta-ext@jci.com>
2023-01-17 21:41:23 +0530

    Updates

---
+++ precache-manifest.5d00581bba9dc63498db077543215330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e861bc7714915fee4ba6542db04b201b",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "b89ed08d23e43de2810c",
+    "url": "/my-resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cadf691a1c62432aac8c",
+    "url": "/my-resume/static/js/2.641ed9ab.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/my-resume/static/js/2.641ed9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89ed08d23e43de2810c",
+    "url": "/my-resume/static/js/main.923a35fb.chunk.js"
+  },
+  {
+    "revision": "12c30c98f0d92364ff47",
+    "url": "/my-resume/static/js/runtime-main.ed4ac1e5.js"
+  }
+]);
